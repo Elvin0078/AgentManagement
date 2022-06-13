19f6a7296e04fcf8a81ab9ff74937f5e239f0691
ELvin0078 <canbaxisovelvin@gmail,com>
2022-06-13 21:05:04 +0400

    .

--- src/Main.java
+++ src/Main.java
@@ -8,55 +8,78 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-        System.out.println("Agent Management System");
-        int counter=0;
-        int i = 0 ;
-        while (true){
+            System.out.println("Agent Management System");
+            int counter = 0;
+            int i = 0;
+            String inputNum = "";
+            String agentProductNum ="";
+            while (true) {
 
-            Scanner in = new Scanner(System.in);
-            System.out.println("Agent username daxil edin ");
-            String username=in.nextLine();
-            System.out.println("Agent parolu daxil edin ");
-            String password=in.nextLine();
-
-            Map usersMap = new HashMap();
-            Map productMap = new HashMap();
-            List<String> productList = new ArrayList<String>() ;
-            usersMap.put("Agent1","A");
-            String userLine;
-            String product;
+                Scanner in = new Scanner(System.in);
+                System.out.println("Agent username daxil edin ");
+                String username = in.nextLine();
+                System.out.println("Agent parolu daxil edin ");
+                String password = in.nextLine();
+                inputNum="";
+                Map usersMap = new HashMap();
+                Map productMap = new HashMap();
+                List<String> productList = new ArrayList<String>();
+                usersMap.put("Agent1", "A");
+                String userLine;
+                String product;
                 BufferedReader bw = new BufferedReader(new FileReader("C:/Agent/Users.txt"));
-                while ((userLine= bw.readLine())!=null) {
-                    usersMap.put(userLine.substring(0,userLine.indexOf("&")),userLine.substring(userLine.indexOf("&")+1) );
+                while ((userLine = bw.readLine()) != null) {
+                    usersMap.put(userLine.substring(0, userLine.indexOf("&")), userLine.substring(userLine.indexOf("&") + 1));
                 }
                 if (usersMap.containsKey(username) && usersMap.get(username).equals(password)) {
-                    System.out.println(username+" sistemə daxil oldu");
-                    System.out.println("Icra etmek istediyiniz emeliyyatin nomresini daxil edin ");
-                    System.out.println("1- Məhsullara baxış");
-                    System.out.println("2- Diger Agentlerin mehsullari");
-                    String inputNum= in.nextLine();
-                    if (inputNum.equals("1")) {
-                        BufferedReader bw2 = new BufferedReader(new FileReader("C:/Agent/"+username+"product.txt"));
-                        while ((product= bw2.readLine())!=null) {
-                            productList = Arrays.asList(product.split("&"));
-                            productMap.put(i,productList);
-                            System.out.printf(productMap.get(i).toString());
-                            i++;
+                    System.out.println(username + " sistemə daxil oldu");
+                    while (!inputNum.equals("0")) {
+                        System.out.println("Icra etmek istediyiniz emeliyyatin nomresini daxil edin ");
+                        System.out.println("0- Sistemdən çıxış");
+                        System.out.println("1- Məhsullara baxış");
+                        System.out.println("2- Diger Agentlerin mehsullarina baxış");
+                        inputNum = in.nextLine();
+                        if (inputNum.equals("1")) {
+                            BufferedReader bw2 = new BufferedReader(new FileReader("C:/Agent/" + username + "product.txt"));
+                            while ((product = bw2.readLine()) != null) {
+                                productList = Arrays.asList(product.split("&"));
+                                productMap.put(i, productList);
+                                System.out.println(productMap.get(i).toString());
+                                i++;
+                            }
+                            i=0;
+
+                        }else if (inputNum.equals("2")){
+                            System.out.println("Mehsullarina baxmaq istədiyiniz Agenti daxil edin ");
+                            BufferedReader bw3 = new BufferedReader(new FileReader("C:/Agent/" + in.nextLine() + "product.txt"));
+                            productMap = new HashMap();
+                            while ((product = bw3.readLine()) != null) {
+                                productList = Arrays.asList(product.split("&"));
+                                productMap.put(i, productList);
+                                System.out.println(i+" "+ productMap.get(i) );
+                                i++;
+                            }
+                            i=0;
+                            System.out.println("Almaq istediyiniz mehsulun nomresini daxil edin ");
+                            String order = in.nextLine();
+                            System.out.println(order + " adlı məhsuldan almaq istədiyiniz miqdarı daxil edin ");
+                            String orderNum = in.nextLine();
+
+
                         }
 
                     }
-
-                }  else
+                } else
                     System.err.println("Istifadəçi adı və ya şifrə yanlışdır");
 
-            counter++;
+                counter++;
 
+            }
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.err.println("Xeta bash verdi...");
         }
-
-
-    }catch (Exception e) {
-        e.printStackTrace();
-        System.err.println("Xeta bash verdi...");
-    }
     }
 }
